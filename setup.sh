c56ca52b4f2892e2b265afdd187ad74de54d7022
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2013-02-04 19:32:35 +0200

    Corrected message using awesome github online edit feature

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ for FILE_NAME in $(find . -type f -name ".*" | sed 's|\./||')
     ln -s ~/.dotfiles/$FILE_NAME ~/$FILE_NAME
   done
 
-echo "Aliaksei Kliuchnikau's dotfiles installed susscesfully"
+echo "Aliaksei Kliuchnikau's dotfiles installed successfully"
