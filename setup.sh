7b32d09174b54aa68746cd7a85db877609b961f5
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2020-12-23 12:10:33 +0100

    Correct url to download git-completion script

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ for FILE_NAME in $(find . -type f -name ".*" | sed 's|\./||')
 
 if [[ $OSTYPE == darwin* ]]; then
   # Mac stuff
-  curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+  curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 
   ln -s ~/.dotfiles/.bashrc ~/.bash_profile
 fi
